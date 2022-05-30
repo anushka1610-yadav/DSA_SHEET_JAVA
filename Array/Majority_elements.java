9a37edfe1243c2f5a791adfbdea2a39e95502e59
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-05-30 19:41:04 +0530

    added

---
+++ Array/Majority_elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int c=0,ele=0;
+        for(int n:nums)
+        {
+            if(c==0)
+                ele=n;
+            if(n==ele)
+                c++;
+            else
+                c--;
+        }
+        return ele;
+        
+    }
+}
