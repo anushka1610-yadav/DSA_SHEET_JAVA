aa270b696a9d7763a4435b122e4734c75c9d06c6
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-06-30 22:27:16 +0530

    added

---
+++ Array/Common_elements.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here
+          ArrayList<Integer> ans = new ArrayList<>();
+       // three sets for three arrays
+       Set<Integer> set1 = getSet(A);
+       Set<Integer> set2 = getSet(B);
+       Set<Integer> set3 = getSet(C);
+
+       for (Integer i : set1) {
+           if (set2.contains(i) && set3.contains(i))
+               ans.add(i);
+       }
+       return ans;
+   }
+
+   public Set<Integer> getSet(int[] a) {
+       Set<Integer> set = new TreeSet<>();
+       for (int i : a)
+           set.add(i);
+       return set;
+   }
+}
