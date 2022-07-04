22414e2cc4ab4aaae7c8baf5e92fe4117c690dff
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-07-04 16:34:42 +0530

    added

---
+++ Array/Minimum_swap_and_k_togethere.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Complete obj = new Complete();
+		    int ans = obj.minSwap(arr, sizeOfArray, K);
+		    System.out.println(ans);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int minSwap (int arr[], int n, int k) {
+        //Complete the function
+        int fav=0, non=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+                fav++;
+        }
+        
+        for(int j=0;j<fav;j++)
+        {
+            if(arr[j]>k)
+                non++;
+        }
+        
+        int l=0,r=fav-1, res=Integer.MAX_VALUE;
+        while(r<n)
+        {
+            res=Math.min(res, non);
+            r++;
+            if(r<n && arr[r]>k) non++;
+            if(l<n && arr[l]>k) non--;
+            l++;
+        }
+        
+        return (res==Integer.MAX_VALUE)?0:res;
+    }
+    
+    
+}
